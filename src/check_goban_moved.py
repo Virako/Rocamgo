3cbd86e5c8a0ad21b9b857acbaba026ed70d6605
Victor Ramirez <virako.9@gmail.com>
2012-08-30 11:18:13 +0200

    Fixes check goban moved (Problem with x==x and y==y). Add profiler. Finish the kifu class.

--- src/check_goban_moved.py
+++ src/check_goban_moved.py
@@ -21,40 +21,75 @@
 from src.cte import *
 from cv import *
 
+def search_sum_pixels_in_line_y(x, y, dist, img1, img2): # if x line: values order x, y
+    change = 0
+    acum = 0
+    while acum < NUM_POINTS:
+        pixel1 = Get2D(img1, y, x)
+        pixel2 = Get2D(img2, y, x)
+        change += abs(pixel1[0]-pixel2[0]) + \
+         abs(pixel1[1]-pixel2[1]) + abs(pixel1[2]-pixel2[2]) 
+        y -= dist 
+        acum += 1
+    return change
+
+def search_sum_pixels_in_line_x(x, y, dist, img1, img2): # if x line: values order x, y
+    change = 0
+    acum = 0
+    while acum < NUM_POINTS:
+        pixel1 = Get2D(img1, y, x)
+        pixel2 = Get2D(img2, y, x)
+        change += abs(pixel1[0]-pixel2[0]) + \
+         abs(pixel1[1]-pixel2[1]) + abs(pixel1[2]-pixel2[2]) 
+        x -= dist 
+        acum += 1
+    return change
+
 def check_goban_moved(img1,img2,corners):
 
     if not (img1 and img2):
         return True 
  
     change_total = []
-    for c in corners[::3] :
-        p1_x = c[0]
-        p1_y = c[1]
+    # TODO Comentar que recorremos las 4 lineas externas para buscar cambios
+    for c1 in corners[::3] :
+        p1_x = c1[0]
+        p1_y = c1[1]
         for c2 in corners[1:3]:            
             p2_x = c2[0]
             p2_y = c2[1]
             
-            dist_x = abs(p1_x - p2_x) / float(NUM_POINTS)
-                                
-            if p1_x < p2_x:
-                p1_x,p2_x = p2_x,p1_x
-                p1_y,p2_y = p2_y,p1_y
-                
-            x = p1_x 
-            y = p1_y
-            change_sum = 0
-            acum = 0
-            while acum <= NUM_POINTS: 
-                pixel = Get2D(img1,int(y),int(x))
-                pixel1 = Get2D(img2,int(y),int(x))
-                change = abs(pixel[0]-pixel1[0]) + abs(pixel[1]-pixel1[1]) +\
-                    abs(pixel[2]-pixel1[2])
-                change_sum += change
-                x = x-dist_x    
-                y = ((x - p1_x)*(p2_y - p1_y) / (p2_x - p1_x))+p1_y
-                acum += 1
-            change_sum /= 20
-            change_total.append(change_sum)
+            if p1_x == p2_x:
+                if p1_y < p2_y:
+                    p1_y, p2_y = p2_y, p1_y
+                dist = (p1_y - p2_y)/NUM_POINTS
+                change = search_sum_pixels_in_line_y(p1_x, p1_y, dist, img1, img2)
+            elif p1_y == p2_y:
+                if p1_x < p2_x:
+                    p1_x,p2_x = p2_x,p1_x
+                dist = (p1_x - p2_x)/NUM_POINTS
+                change = search_sum_pixels_in_line_x(p1_x, p1_y, dist, img1, img2)
+            else:
+                if p1_x < p2_x:
+                    p1_x,p2_x = p2_x,p1_x
+                dist_x = (p1_x - p2_x)/NUM_POINTS
+                x = p1_x 
+                y = p1_y
+                change = 0
+                acum = 0
+                while acum < NUM_POINTS: 
+                    pixel1 = Get2D(img1, y, x)
+                    pixel2 = Get2D(img2, y, x)
+                    change += abs(pixel1[0]-pixel2[0]) + \
+                     abs(pixel1[1]-pixel2[1]) + abs(pixel1[2]-pixel2[2]) 
+                    x -= dist_x 
+                    y = (((x - p1_x)*(p2_y - p1_y))/(p2_x - p1_x))+p1_y
+                    acum += 1
+            change /= 20
+            change_total.append(change)
+            # add to test goban with x1==x2 and y1==y2. No hay pendiente. 
 
-    return min(change_total) > 35 and max(change_total) > 85
+    print change_total, corners
+    #return min(change_total) > 30 and max(change_total) > 80
+    return sum(change_total) > 40*4
 
